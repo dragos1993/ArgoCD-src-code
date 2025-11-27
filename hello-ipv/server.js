eb0388bd6eb4991b70ee9a075a622098917fce6a
Dragos Vrinceanu <dravrinc@publicisgroupe.net>
2025-11-27 16:20:34 +0200

    t

---
+++ hello-ipv/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ipv app 👋');
+});
+
+app.listen(port, () => {
+  console.log(`ipv app listening on port ${port}`);
+});
